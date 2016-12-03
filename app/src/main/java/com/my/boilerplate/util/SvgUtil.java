175d1bdbb8f047c5b3a6eb9985c2603e958a86a5
Boy Wang <boyw165@gmail.com>
2016-12-04 00:01:03 +0800

    Support the non-zero rule for inside/outside testing.

--- app/src/main/java/com/my/boilerplate/util/SvgUtil.java
+++ app/src/main/java/com/my/boilerplate/util/SvgUtil.java
@@ -26,28 +26,53 @@ import java.util.List;
 
 public class SvgUtil {
 
-    public static boolean containsPoint(List<PointF> closedPath,
-                                        PointF testPt) {
+    public static final int RAY_CASTING_BY_EVEN_ODD = 0;
+    public static final int RAY_CASTING_BY_NON_ZERO = 1;
+
+    /**
+     * Tell if a testing point is on the inside or outside of a closed path.
+     *
+     * @param closedPath A list of points representing a closed path.
+     * @param testPt The testing point.
+     * @param rayCastingMethod RAY_CASTING_BY_EVEN_ODD | RAY_CASTING_BY_NON_ZERO
+     */
+    public static boolean ifPathContainsPoint(List<PointF> closedPath,
+                                              PointF testPt,
+                                              int rayCastingMethod) {
         if (closedPath == null ||
             testPt == null ||
             closedPath.size() < 2) return false;
 
-        boolean isIntersected = false;
-        for (int end = 0, start = closedPath.size() - 1;
-             end < closedPath.size(); end++) {
-            if (ifRayCrossesPathSegment(testPt,
-                                        closedPath.get(start),
-                                        closedPath.get(end))) {
-                isIntersected = !isIntersected;
+        if (rayCastingMethod == RAY_CASTING_BY_EVEN_ODD) {
+            // The winding counter in clockwise and counter-clockwise direction.
+            boolean ifInside = false;
+            for (int end = 0, start = closedPath.size() - 1;
+                 end < closedPath.size(); end++) {
+                if (ifRayCrossesPathSegmentByEvenOdd(testPt,
+                                                     closedPath.get(start),
+                                                     closedPath.get(end))) {
+                    ifInside = !ifInside;
+                }
+                start = end;
             }
-            start = end;
+
+            return ifInside;
+        } else if (rayCastingMethod == RAY_CASTING_BY_NON_ZERO) {
+            // The winding counter in clockwise and counter-clockwise direction.
+            int windingCounter = 0;
+            for (int end = 0, start = closedPath.size() - 1;
+                 end < closedPath.size(); end++) {
+                windingCounter += ifRayCrossesPathSegmentByNonZero(testPt,
+                                                                   closedPath.get(start),
+                                                                   closedPath.get(end));
+                start = end;
+            }
+
+            return windingCounter != 0;
         }
 
-        return isIntersected;
+        return false;
     }
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Protected / Private Methods ////////////////////////////////////////////
 
     /**
      * According to the even-odd rule, you draw a ray from the testing point
@@ -56,8 +81,8 @@ public class SvgUtil {
      * In our case, we draw the ray from the testing point to infinity in the
      * horizontal direction.
      * <br/>
-     * e.g.
      * <pre>
+     *
      * ^                    ^
      * |       2            |  1
      * |      /             |   \
@@ -84,10 +109,13 @@ public class SvgUtil {
      * 2) the testing x should always be on the left side of the path
      * segment.
      * </pre>
+     * Reference:
+     * <br/>
+     * https://en.wikipedia.org/wiki/Even%E2%80%93odd_rule
      */
-    public static boolean ifRayCrossesPathSegment(PointF rayStartPt,
-                                                  PointF pathPt1,
-                                                  PointF pathPt2) {
+    private static boolean ifRayCrossesPathSegmentByEvenOdd(PointF rayStartPt,
+                                                            PointF pathPt1,
+                                                            PointF pathPt2) {
         if (rayStartPt.y == pathPt1.y && pathPt1.y == pathPt2.y) {
             // Horizontal line case. In case that the denominator is 0 when
             // calculating the slope reciprocal.
@@ -112,5 +140,97 @@ public class SvgUtil {
         } else {
             return false;
         }
+    }
+
+    /**
+     * The basic concept is totally the same with the
+     * {@code ifRayCrossesPathSegmentByEvenOdd}. Further more, this algorithm
+     * takes the winding direction into account too.
+     * <br/>
+     * <pre>
+     *
+     * 1) How to tell if the ray crosses the path segment?
+     *
+     * ^                    ^
+     * |       2            |  1
+     * |      /             |   \
+     * |  *--/-------...    | *--\--------... Ray from the testing point to
+     * |    /  *-----...    |     \  *----... infinity.
+     * |   1                |      2
+     * +----------->        +----------->
+     *   slope > 0     and    slope < 0
+     *
+     *  y - y1     y2 - y1
+     * -------- = --------- = pathSlope
+     *  x - x1     x2 - x1
+     *
+     * becomes...
+     *
+     *       y - y1
+     * x = ---------- + x1
+     *      pathSlope
+     *
+     * 2) How to tell if the path segment is in clockwise and counter-clockwise
+     * winding?
+     *
+     * The clockwise examples:
+     * ^               ^               ^
+     * |   2           |      2        |
+     * |   |           |     /         |
+     * |   |           |    /          |
+     * |   |           |   /           |
+     * |   1           |  1            |  1-----2
+     * +----------->   +----------->   +----------->
+     *
+     * The counter-clockwise examples:
+     * ^               ^               ^
+     * |   1           |      1        |
+     * |   |           |     /         |
+     * |   |           |    /          |
+     * |   |           |   /           |
+     * |   2           |  2            |  2-----1
+     * +----------->   +----------->   +----------->
+     *
+     * </pre>
+     * Reference:
+     * <br/>
+     * https://en.wikipedia.org/wiki/Nonzero-rule
+     *
+     * @return 0 = no cross;
+     * +1 = cross in clockwise winding direction;
+     * -1 = cross in counter-clockwise winding direction.
+     */
+    private static int ifRayCrossesPathSegmentByNonZero(PointF rayStartPt,
+                                                        PointF pathPt1,
+                                                        PointF pathPt2) {
+        if (pathPt1.equals(pathPt2)) return 0;
+
+        // +1 = clockwise.
+        // -1 = counter-clockwise.
+        int windingCounter = 0;
+        if (rayStartPt.y == pathPt1.y && pathPt1.y == pathPt2.y &&
+            rayStartPt.x <= Math.max(pathPt2.x, pathPt1.x)) {
+            // Horizontal line case. In case that the denominator is 0 when
+            // calculating the slope reciprocal.
+            // Clockwise:         x2 > x1;
+            // Counter-clockwise: x2 < x1;
+            windingCounter = (int) Math.signum(pathPt1.x - pathPt2.x);
+        } else if ((pathPt1.y >= rayStartPt.y) != (pathPt2.y >= rayStartPt.y)) {
+            // It's not a horizontal line and the testing y is in the range
+            // of the path segment.
+            if (rayStartPt.x <= Math.max(pathPt1.x, pathPt2.x)) {
+                // The ray always doesn't crosses with the path segment if the
+                // starting x is larger than the minimum x value of the path
+                // segment.
+                // Clockwise:         y2 > y1;
+                // Counter-clockwise: y2 < y1;
+                float pathSlope = (pathPt2.y - pathPt1.y) / (pathPt2.x - pathPt1.x);
+                if (rayStartPt.x <= (rayStartPt.y - pathPt1.y) / pathSlope + pathPt1.x) {
+                    windingCounter = (int) Math.signum(pathPt2.y - pathPt1.y);
+                }
+            }
+        }
+
+        return windingCounter;
     }
 }
