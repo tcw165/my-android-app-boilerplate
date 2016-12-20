de1be5dcefd77a6d4a1a853d00fd24d8571bfed6
Boy Wang <boyw165@gmail.com>
2016-12-20 22:00:07 +0800

    Trying to generalize the touch interface.

--- app/src/main/java/com/my/boilerplate/view/MyGestureDetector.java
+++ app/src/main/java/com/my/boilerplate/view/MyGestureDetector.java
@@ -5,10 +5,17 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public abstract class MyGestureDetector {
+
+    public abstract boolean canHandle(View v,
+                                      int action, MotionEvent viewEvent,
+                                      MotionEvent rootEvent);
+
     public abstract void startSession(View v,
                                       MotionEvent viewEvent,
                                       MotionEvent rootEvent);
+
     public abstract void stopSession();
+
     public abstract Matrix getTransformMatrix(View v,
                                               MotionEvent viewEvent,
                                               MotionEvent rootEvent);
