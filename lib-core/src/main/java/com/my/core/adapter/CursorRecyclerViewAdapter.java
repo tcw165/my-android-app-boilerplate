e3edbc484e153d459cc26cda0467aa9c9e391ed7
Boy Wang <boyw165@gmail.com>
2017-04-11 11:05:33 +0800

    Add document.

--- lib-core/src/main/java/com/my/core/adapter/CursorRecyclerViewAdapter.java
+++ lib-core/src/main/java/com/my/core/adapter/CursorRecyclerViewAdapter.java
@@ -55,6 +55,7 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
         mDataValid = false;
         mRowIdColumn = -1;
 
+        // Enable stable-ID so that it is able to find ViewHolder by item ID.
         setHasStableIds(true);
     }
 
@@ -189,6 +190,25 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
         return mRecyclerView.findViewHolderForItemId(id);
     }
 
+    /**
+     * Return the ViewHolder for the item in the given position of the data set.
+     * Unlike {@link RecyclerView#findViewHolderForLayoutPosition(int)} this
+     * method takes into account any pending adapter changes that may not be
+     * reflected to the layout yet. On the other hand, if
+     * {@link RecyclerView.Adapter#notifyDataSetChanged()}
+     * has been called but the new layout has not been calculated yet, this
+     * method will return <code>null</code> since the new positions of views are
+     * unknown until the layout is calculated.
+     * <p>
+     * This method checks only the children of RecyclerView. If the item at the given
+     * <code>position</code> is not laid out, it <em>will not</em> create a new one.
+     * <p>
+     * When the ItemAnimator is running a change animation, there might be 2 ViewHolders
+     * representing the same Item. In this case, the updated ViewHolder will be returned.
+     *
+     * @param position The position of the item in the data set of the adapter
+     * @return The ViewHolder at <code>position</code> or null if there is no such item
+     */
     @SuppressWarnings("unused")
     final public RecyclerView.ViewHolder findViewHolderForAdapterPosition(final int position) {
         if (mRecyclerView == null) {
