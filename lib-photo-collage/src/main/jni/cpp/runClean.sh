6324e311aacf32ab160ae8249731b15b55aef628
Boy Wang <boyw165@gmail.com>
2017-04-15 16:02:52 +0800

    Refactoring: CMake build would be processed under build directory.

--- lib-photo-collage/src/main/jni/cpp/runClean.sh
+++ lib-photo-collage/src/main/jni/cpp/runClean.sh
@@ -5,4 +5,5 @@ rm -rf CMakeFiles > /dev/null 2>&1
 rm CMakeCache.txt > /dev/null 2>&1
 rm CTestTestfile.cmake > /dev/null 2>&1
 rm cmake_install.cmake > /dev/null 2>&1
+rm build > /dev/null 2>&1
 echo "Done!"
