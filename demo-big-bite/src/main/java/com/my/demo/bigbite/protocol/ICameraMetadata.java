c60ccc93586d813bd0ffee5b4f73209391945402
Boy Wang <boyw165@gmail.com>
2017-05-21 23:06:20 +0800

    Init project big-bite. #55

---
+++ demo-big-bite/src/main/java/com/my/demo/bigbite/protocol/ICameraMetadata.java
@@ -0,0 +1,12 @@
+package com.my.demo.bigbite.protocol;
+
+public interface ICameraMetadata {
+
+    boolean isFacingFront();
+
+    boolean isFacingBack();
+
+    boolean isPortraitMode();
+
+    boolean isLandscapeMode();
+}
